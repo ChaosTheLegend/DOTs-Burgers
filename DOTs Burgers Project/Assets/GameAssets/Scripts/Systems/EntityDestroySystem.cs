10496faf9aa76cced7bc8345ec3dc9b27ac6d33d
Ivan Yakimov <legendthechaos@gmail.com>
2023-06-24 18:01:04 +0600

    Refactored Destroy system for burst

--- DOTs Burgers Project/Assets/GameAssets/Scripts/Systems/EntityDestroySystem.cs
+++ DOTs Burgers Project/Assets/GameAssets/Scripts/Systems/EntityDestroySystem.cs
@@ -2,10 +2,29 @@
 using GameAssets.Scripts.Components;
 using GameAssets.Scripts.Temp;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 
 namespace GameAssets.Scripts.Systems
 {
+    public partial struct EntityDestroyJob : IJobEntity
+    {
+        public EntityCommandBuffer.ParallelWriter EntityCommandBuffer;
+
+        [BurstCompile]
+        private void Execute(DestroyTagAspect destroyTag)
+        {
+            EntityCommandBuffer.DestroyEntity(0, destroyTag.entity);
+        }
+    }
+    
+    public readonly partial struct DestroyTagAspect : IAspect
+    {
+        public readonly Entity entity;
+        
+        public readonly RefRO<DestroyedTagComponent> ValueRO;
+    }
+    
     public partial struct EntityDestroySystem : ISystem
     {
         [BurstCompile]
@@ -17,7 +36,17 @@ namespace GameAssets.Scripts.Systems
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            state.EntityManager.DestroyEntity(state.EntityManager.CreateEntityQuery(typeof(DestroyedTagComponent)));
+            var entityCommandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+            var parallelWriter = entityCommandBuffer.AsParallelWriter();
+            
+            var job = new EntityDestroyJob
+            {
+                EntityCommandBuffer = parallelWriter
+            };
+            
+            job.ScheduleParallel(state.Dependency).Complete();
+            
+            entityCommandBuffer.Playback(state.EntityManager);
         }
 
         [BurstCompile]
